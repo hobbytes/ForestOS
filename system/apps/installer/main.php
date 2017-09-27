04f09a5c0ef346a96cad806ae0811d394f54177f
Vyacheslav Gorodilov <gvgsochi@gmail.com>
2017-09-27 12:12:50 +0300

    installer localized

--- system/apps/installer/main.php
+++ system/apps/installer/main.php
@@ -1,5 +1,5 @@
 <?$appname=$_GET['appname'];$appid=$_GET['appid'];?>
-<div id="<?echo $appname.$appid;?>" style="background-color:#ebebeb; height:300px; width:400px; color:#000; max-height:95%; max-width:100%; border-radius:0px 0px 5px 5px; overflow:auto;">
+<div id="<?echo $appname.$appid;?>" style="background-color:#ebebeb; height:230px; width:400px; color:#000; max-height:95%; max-width:100%; border-radius:0px 0px 5px 5px; overflow:auto;">
 <?php
 /*Application Installer*/
 //Подключаем библиотеки
@@ -15,6 +15,9 @@ $nameappdownload=str_replace('_',' ',$appdownload);
 //Запускаем сессию
 session_start();
 $security->appprepare();
+//Загружаем файл локализации
+$install_lang  = parse_ini_file('app.lang');
+$cl = $_SESSION['locale'];
 //Логика
 $appinstall=$_GET['appinstall'];
 
@@ -44,42 +47,45 @@ if(isset($appinstall)){
   fclose($fp);
 $zip=new ZipArchive;
 if($zip->open('./temp/'.$appinstall.$temphash.'.zip') === TRUE){
-$zip->extractTo('../../../'.str_replace($appinstall,'',$_GET['appinstdest']));
-$zip->close();
-$appname=str_replace("_"," ", $appinstall);
-$myfile=fopen('../../users/'.$_SESSION["loginuser"].'/desktop/'.$appinstall.'.link',"w");
-$content="[link]\ndestination=".$_GET['appinstdest']."/\nfile=main\nkey=\nparam=\nname='$appinstall'\nlinkname='$appinstall'\n";
-fwrite($myfile,$content);fclose($myfile);
 
+  $zip->extractTo('../../../'.str_replace($appinstall,'',$_GET['appinstdest']));
+  $zip->close();
+  $appname=str_replace("_"," ", $appinstall);
+  $myfile=fopen('../../users/'.$_SESSION["loginuser"].'/desktop/'.$appinstall.'.link',"w");
+  $content="[link]\ndestination=".$_GET['appinstdest']."/\nfile=main\nkey=\nparam=\nname='$appinstall'\nlinkname='$appinstall'\n";
+  fwrite($myfile,$content);fclose($myfile);
 
-$ini_array = parse_ini_file('../../core/appinstall.foc', true);
-if (array_key_exists($appinstall, $ini_array))
-{
-  config_set('../../core/appinstall.foc', $appinstall, 'version', $_SESSION['appversion']);
-  $type="обнолено"; $type2="Обновление";
-}
-else
-{
-$myfile=fopen('../../core/appinstall.foc',"a");
-$content='['.$appinstall.']'.PHP_EOL.'version='.$_SESSION['appversion'].PHP_EOL.'destination='.$_GET['appinstdest'].PHP_EOL;
-fwrite($myfile,PHP_EOL.$content);fclose($myfile);
-unlink('./temp/'.$appinstall.$temphash.'.zip');
-$type="установлено"; $type2="Установка";
-}
-$gui->newnotification($appname,$type2,'Приложение '.$appinstall.' '.$type.'!');?><script>$(function(){$("#process<?echo $appid;?>").remove();});</script><?}else{$gui->newnotification($appname,'Установка','Приложение '.$appinstall.' не установлено!'); ?>
-<script>$(function(){$("#process<?echo $appid;?>").remove();});</script><?}
+  $ini_array = parse_ini_file('../../core/appinstall.foc', true);
+  if (array_key_exists($appinstall, $ini_array))
+  {
+    config_set('../../core/appinstall.foc', $appinstall, 'version', $_SESSION['appversion']);
+    $type=$install_lang[$cl.'_installer_msg_upd_1']; $type2=$install_lang[$cl.'_installer_msg_upd_2'];
+  }else{
+    $myfile=fopen('../../core/appinstall.foc',"a");
+    $content='['.$appinstall.']'.PHP_EOL.'version='.$_SESSION['appversion'].PHP_EOL.'destination='.$_GET['appinstdest'].PHP_EOL;
+    fwrite($myfile,PHP_EOL.$content);fclose($myfile);
+    unlink('./temp/'.$appinstall.$temphash.'.zip');
+    $type=$install_lang[$cl.'_installer_msg_1']; $type2=$install_lang[$cl.'_installer_msg_2'];
+  }
+  $pubname = str_replace('_',' ',$appinstall);
+  $gui->newnotification($appname,$type2,$install_lang[$cl.'_installer_msg_label'].' <b>'.$pubname.'</b> '.$type.'!');?>
+  <script>$(function(){$("#process<?echo $appid;?>").remove();});</script>
+<?}else{
+  $gui->newnotification($appname,$install_lang[$cl.'_installer_msg_2'],$install_lang[$cl.'_installer_msg_label'].' <b>'.$pubname.'</b> '.$install_lang[$cl.'_installer_msg_label_2']); ?>
+  <script>$(function(){$("#process<?echo $appid;?>").remove();});</script>
+  <?}
 }
 else{
 ?>
 <p style="text-align:center"><div style="background-image: url(http://forest.hobbytes.com/media/os/apps/<?echo $appdownload;?>/app.png); background-size:cover; margin:auto; height:64px; width:64px;"></div></p>
 <div style="text-align:center; font-size:20px;">
-  Установка приложения <b><?echo $nameappdownload;?></b>
+  <?echo $install_lang[$cl.'_installer_label']?>: <b><?echo $nameappdownload;?></b>
 </div>
-<div style="margin-top:30px; margin-left:8%;"><label for="destinput<?echo $appid;?>">Путь для установки:</label> <input id="destinput<?echo $appid;?>" type="text" value="system/apps/<?echo $appdownload;?>/"></div>
+<div style="margin-top:30px; margin-left:8%; display:none;"><label for="destinput<?echo $appid;?>">Путь для установки:</label> <input id="destinput<?echo $appid;?>" type="text" value="system/apps/<?echo $appdownload;?>/"></div>
 <label style="margin-top:10px; display:none; margin-left:8%;" for="checkbox<?echo $appid;?>" >Создать ярлык
 <input type="checkbox" name="checkbox<?echo $appid;?>" id="checkbox<?echo $appid;?>" ></input></label>
 <br>
-<div id="<?echo $appdownload;?>" onClick="appinstall<?echo $appid;?>(this);" style="background-color:#54c45c; color:#fff; width:200px; font-size:20px; text-align:center; margin-left:25%; cursor:pointer; padding:5px;">Установить</div>
+<div id="<?echo $appdownload;?>" onClick="appinstall<?echo $appid;?>(this);" style="background-color:#54c45c; color:#fff; width:200px; font-size:20px; text-align:center; margin-left:25%; cursor:pointer; padding:5px;"><?echo $install_lang[$cl.'_installer_button']?></div>
 <?}?>
 </div>
 <script>
