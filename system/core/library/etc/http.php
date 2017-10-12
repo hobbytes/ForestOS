91bda7bfd61723068b63e33a1247afaad0798291
Vyacheslav Gorodilov <gvgsochi@gmail.com>
2017-10-12 10:46:46 +0300

    all new library

--- system/core/library/etc/http.php
+++ system/core/library/etc/http.php
@@ -2,19 +2,19 @@
 
 class http{
 
-  function makeNewRequest($url,$data,$method){
-    $options = array(
-  'http' => array(
-    'header'  => "Content-type: application/x-www-form-urlencoded\r\n",
-    'method'  => $method,
-    'content' => http_build_query($data),
-  ),
-);
-$context  = stream_context_create($options);
-$result = file_get_contents($url, false, $context);
-return $result;
+  function makeNewRequest($url, $agent, $data){
+    $curl = curl_init();
+    curl_setopt_array($curl, array(
+        CURLOPT_RETURNTRANSFER => 1,
+        CURLOPT_URL => $url,
+        CURLOPT_USERAGENT => $agent,
+        CURLOPT_POST => 1,
+        CURLOPT_POSTFIELDS => $data
+    ));
+    $resp = curl_exec($curl);
+    curl_close($curl);
+    return $resp;
   }
-
 }
 
 ?>
