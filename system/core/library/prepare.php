452598c509104be73a81bd07be701eb6149c5975
Vyacheslav Gorodilov <gvgsochi@gmail.com>
2017-10-13 16:18:22 +0300

    Увеличил верхний бар

--- system/core/library/prepare.php
+++ system/core/library/prepare.php
@@ -195,7 +195,7 @@ function hibernation(){
 function topbar(){
   global $object, $login, $language;
   ?>
-  <div id="topbar" class="ui-widget-content topbartheme" style="display:none; z-index:9999; height:22px; padding-top:4px;">
+  <div id="topbar" class="ui-widget-content topbartheme" style="display:none; z-index:9999; height:25px; padding-top:5px;">
     <span id="hideall" class="topbaractbtn ui-forest" style="cursor:pointer; display:none; background-color:#37a22e; color:#fff; width:12px; float:right; text-align:center; width:15px; margin-right: 8px; font-family:monospace; padding:2px 0;">
       -
     </span>
