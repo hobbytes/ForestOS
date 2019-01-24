31620d830f8184e882ee015805dea5dadbbdcc54
Vyacheslav Gorodilov <gvgsochi@gmail.com>
2019-01-24 15:12:45 +0300

    соски сжимаются

--- system/core/library/etc.php
+++ system/core/library/etc.php
@@ -4,22 +4,22 @@ date_default_timezone_set('Europe/Moscow');
 
 class info{
     function browser($agent) {
-    	preg_match("/(MSIE|Opera|Firefox|Chrome|Version|Opera Mini|Netscape|Konqueror|SeaMonkey|Camino|Minefield|Iceweasel|K-Meleon|Maxthon)(?:\/| )([0-9.]+)/", $agent, $browser_info); // регулярное выражение, которое позволяет отпределить 90% браузеров
-            list(,$browser,$version) = $browser_info; // получаем данные из массива в переменную
-            if (preg_match("/Opera ([0-9.]+)/i", $agent, $opera)) return 'Opera '.$opera[1]; // определение _очень_старых_ версий Оперы (до 8.50), при желании можно убрать
-            if ($browser == 'MSIE') { // если браузер определён как IE
-                    preg_match("/(Maxthon|Avant Browser|MyIE2)/i", $agent, $ie); // проверяем, не разработка ли это на основе IE
-                    if ($ie) return $ie[1].' based on IE '.$version; // если да, то возвращаем сообщение об этом
-                    return 'IE '.$version; // иначе просто возвращаем IE и номер версии
+    	preg_match("/(MSIE|Opera|Firefox|Chrome|Version|Opera Mini|Netscape|Konqueror|SeaMonkey|Camino|Minefield|Iceweasel|K-Meleon|Maxthon)(?:\/| )([0-9.]+)/", $agent, $browser_info);
+            list(,$browser,$version) = $browser_info;
+            if (preg_match("/Opera ([0-9.]+)/i", $agent, $opera)) return 'Opera '.$opera[1];
+            if ($browser == 'MSIE') {
+                preg_match("/(Maxthon|Avant Browser|MyIE2)/i", $agent, $ie);
+                if ($ie) return $ie[1].' based on IE '.$version;
+                return 'IE '.$version;
             }
-            if ($browser == 'Firefox') { // если браузер определён как Firefox
-                    preg_match("/(Flock|Navigator|Epiphany)\/([0-9.]+)/", $agent, $ff); // проверяем, не разработка ли это на основе Firefox
-                    if ($ff) return $ff[1].' '.$ff[2]; // если да, то выводим номер и версию
+            if ($browser == 'Firefox') {
+                    preg_match("/(Flock|Navigator|Epiphany)\/([0-9.]+)/", $agent, $ff);
+                    if ($ff) return $ff[1].' '.$ff[2];
             }
-            if ($browser == 'Opera' && $version == '9.80') return 'Opera '.substr($agent,-5); // если браузер определён как Opera 9.80, берём версию Оперы из конца строки
-            if ($browser == 'Version') return 'Safari '.$version; // определяем Сафари
-            if (!$browser && strpos($agent, 'Gecko')) return 'Browser based on Gecko'; // для неопознанных браузеров проверяем, если они на движке Gecko, и возращаем сообщение об этом
-            return $browser.' '.$version; // для всех остальных возвращаем браузер и версию
+            if ($browser == 'Opera' && $version == '9.80') return 'Opera '.substr($agent,-5);
+            if ($browser == 'Version') return 'Safari '.$version;
+            if (!$browser && strpos($agent, 'Gecko')) return 'Browser based on Gecko';
+            return $browser.' '.$version;
     }
 
     function writestat($alarmbody,$folder){
@@ -57,8 +57,7 @@ class info{
       global $getdata, $getstat;
       $security = new security;
       $bd = new readbd;
-      $bd->readglobal2("password","forestusers","status",superuser);
-      $key = $getdata;
+      $key = $bd->readglobal2("password", "forestusers", "status", superuser, true);
       $content  = file_get_contents($folder);
       $getstat = $security->__decode($content, $key);
     }
@@ -69,7 +68,14 @@ class info{
       if(!isset($_SESSION)){
         session_start();
       }
+
       $login = $_SESSION['loginuser'];
+      if(!is_dir($_SERVER['DOCUMENT_ROOT'].'/system/users/'.$login)){
+        session_destroy();
+        header('Location: os.php?action=logout');
+        exit;
+      }
+      
       $language  = parse_ini_file($_SERVER['DOCUMENT_ROOT'].'/system/core/os.lang');
       if(!empty($login)){
         $fuid = $bd->readglobal2("fuid", "forestusers", "login", $login, true);
