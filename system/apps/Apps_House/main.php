b3da01c58ae55adc6eadf9a51ee4141f5f7eea40
Vyacheslav Gorodilov <gvgsochi@gmail.com>
2019-02-20 12:03:08 +0300

    ненавистная локализация

--- system/apps/Apps_House/main.php
+++ system/apps/Apps_House/main.php
@@ -28,6 +28,9 @@ $AppContainer->customStyle = 'padding-top:0px; max-width:100%;';
 //$AppContainer->showError = true;
 $AppContainer->StartContainer();
 
+//load language
+$language = parse_ini_file('assets/lang/'.$_SESSION['locale'].'.lang');
+
 /* get data */
 $activeTab = 0;
 if(isset($_GET['activetab'])){
@@ -68,17 +71,17 @@ if(!empty($AppSearch)){
 
 <div id="Tabs<?echo $AppID?>">
   <ul>
-    <li><a href="#Apps<?echo $AppID?>">Приложения</a></li>
-    <li><a href="#Control<?echo $AppID?>">Личный кабинет</a></li>
-    <li><a href="#Updates<?echo $AppID?>">Обновления</a></li>
+    <li><a href="#Apps<?echo $AppID?>"><? echo $language['app_tab'] ?></a></li>
+    <li><a href="#Control<?echo $AppID?>"><? echo $language['control_tab'] ?></a></li>
+    <li><a href="#Updates<?echo $AppID?>"><? echo $language['update_tab'] ?></a></li>
   </ul>
 
 <div id="Apps<? echo $AppID ?>" style="margin: 0 auto; overflow: auto;">
-  <div id="TabTitle">Приложения</div>
+  <div id="TabTitle"><? echo $language['app_tab'] ?></div>
 
   <div style="text-align: center; border-bottom: 1px solid #d4d4d4;">
-    <input style="-webkit-appearance: none; padding: 5px; border: 1px solid #ccc; border-radius: 6px; width: 270px; margin-bottom: 20px;" id="AppSearch<? echo $AppID ?>" type="search" value="<? echo $_AppSearch ?>" placeholder="Поиск">
-    <div class="ui-forest-blink" onClick="AppSearch<? echo $AppID ?>()" style="display: inline-block; background: #2196F3; color: #fff; padding: 5px; border-radius: 6px; cursor: default;">Найти</div>
+    <input style="-webkit-appearance: none; padding: 5px; border: 1px solid #ccc; border-radius: 6px; width: 270px; margin-bottom: 20px;" id="AppSearch<? echo $AppID ?>" type="search" value="<? echo $_AppSearch ?>" placeholder="<? echo $language['placeholder_search'] ?>">
+    <div class="ui-forest-blink" onClick="AppSearch<? echo $AppID ?>()" style="display: inline-block; background: #2196F3; color: #fff; padding: 5px; border-radius: 6px; cursor: default;"><? echo $language['button_search'] ?></div>
   </div>
 
   <div style="margin-top: 15px">
@@ -133,14 +136,14 @@ if(!empty($AppSearch)){
             $HttpRequest->makeNewRequest($server_url.'StatApp.php', 'Forest OS', $data = array('login' => $_SESSION["loginuser"], 'token' => "$token", 'hash' => $_GET['install_app_hash'], 'action' => 'install'));
           }
 
-          $InstallCaption_1 = 'Установка';
-          $InstallCaption_2 = 'установлено';
+          $InstallCaption_1 = $language['install_caption_1'];
+          $InstallCaption_2 = $language['install_caption_2'];
         }else{
-          $InstallCaption_1 = 'Обновление';
-          $InstallCaption_2 = 'обновлено';
+          $InstallCaption_1 = $language['update_caption_1'];
+          $InstallCaption_2 = $language['update_caption_2'];
         }
 
-        $gui->newnotification($AppName, $InstallCaption_1.' приложения', 'Приложение <b>'.$pubname.'</b> '.$InstallCaption_2);
+        $gui->newnotification($AppName, $InstallCaption_1.' '.$language['install_n_1'], $language['install_n_2'].' <b>'.$pubname.'</b> '.$InstallCaption_2);
 
       }
     }
@@ -207,7 +210,7 @@ if(!empty($AppSearch)){
 
       //print_r($key);
 
-      $rating = 'Рейтинг: '.getRating($key['rating'], $MaxRating).' ('.$key['rating'].')';
+      $rating = $language['rating_caption'].': '.getRating($key['rating'], $MaxRating).' ('.$key['rating'].')';
 
       $TempIconHash = md5($AppHash.$key['version']);
       $AppIcon = $server_url.'Apps/'.$key['hash'].'/app.png?h='.$TempIconHash;
@@ -218,10 +221,10 @@ if(!empty($AppSearch)){
 
       if (in_array($key['name'], $InstalledApps)) {
         $ButtonClass = "A-button-open";
-        $ButtonCaption = "Открыть";
+        $ButtonCaption = $language['app_button_open'];
       }else{
         $ButtonClass = "A-button-install";
-        $ButtonCaption = "Установить";
+        $ButtonCaption = $language['app_button_install'];
       }
 
       echo '<div class="AppTile" onClick="showInfo'.$AppID.'(\''.$AppHash.'\')">';
@@ -232,10 +235,10 @@ if(!empty($AppSearch)){
       echo $AppName_;
       echo '</div>';
       echo '<span>';
-      echo 'Автор: '.$key['author'].'<br>';
-      echo 'Версия: '.$key['version'].'<br>';
+      echo $language['autor_caption'].': '.$key['author'].'<br>';
+      echo $language['version_caption'].': '.$key['version'].'<br>';
       echo '<div class="AppTile-rating-container">'.$rating.'</div>';
-      echo 'Размер: '.$size.'<br>';
+      echo $language['size_caption'].': '.$size.'<br>';
       echo '</span>';
       echo '</div>';
       echo '</div>';
@@ -250,14 +253,14 @@ if(!empty($AppSearch)){
 
       if($CurrentVersionOS < $key['os_version']){
         echo '<div class="AppTile-button A-button-open">';
-        echo 'Необходима более новая версия ОС';
+        echo $language['os_need_update'];
       }else{
         echo '<div update="false"  app="'.$key['name'].'" app_second="'.$key['second_name'].'" hash="'.$AppHash.'" class="AppTile-button '.$ButtonClass.'">';
         echo $ButtonCaption;
       }
       echo '</div>';
       echo '</div>';
-      echo '<span style="font-size:13px; color:#464646; font-weight:600; padding-top: 5px;">Описание</span>';
+      echo '<span style="font-size:13px; color:#464646; font-weight:600; padding-top: 5px;">'.$language['description_caption'].'</span>';
       echo '<div class="AppTile-description">';
       echo $description;
       echo '</div>';
@@ -271,7 +274,7 @@ if(!empty($AppSearch)){
 
   $MoreApps = $GetPage + 5;
   if($GetApps['Page']['All'] > $GetPage){
-    echo '<span style="padding: 0px 5px; cursor: default; background: #03A9F4; color: #fff; border-radius: 10px;" class="ui-forest-blink" onClick="LoadPage'.$AppID.'('.$MoreApps.');">Еще</span>';
+    echo '<span style="padding: 0px 5px; cursor: default; background: #03A9F4; color: #fff; border-radius: 10px;" class="ui-forest-blink" onClick="LoadPage'.$AppID.'('.$MoreApps.');">'.$language['more_apps'].'</span>';
   }
 
   ?>
@@ -280,12 +283,12 @@ if(!empty($AppSearch)){
 </div>
 
 <div id="Control<? echo $AppID ?>" style="margin: 0 auto;">
-  <div id="TabTitle">Личный Кабинет</div>
+  <div id="TabTitle"><? echo $language['control_tab'] ?></div>
   <div style="padding: 10px; border-bottom: 2px dashed #ccc;">
     <?
     echo '
     <div style="text-align:left; margin-bottom: 10px">
-    <b style="font-size:20px;">Ваши приложения</b>
+    <b style="font-size:20px;">'.$language['my_apps_caption'].'</b>
     </div>
     ';
 
@@ -300,8 +303,8 @@ if(!empty($AppSearch)){
     }
 
     echo '</select>';
-    echo '<div id="SelectEditApp'.$AppID.'" onClick="SelectEditApp'.$AppID.'();" class="ui-forest-button ui-forest-accept" style="margin:10 0;"> Выбрать </div>';
-    echo '<div style="font-weight: 900;font-variant: all-petite-caps;font-size: 18px;">Всего: <b>'.$CountApps.'</b></div>';
+    echo '<div id="SelectEditApp'.$AppID.'" onClick="SelectEditApp'.$AppID.'();" class="ui-forest-button ui-forest-accept" style="margin:10 0;"> '.$language['select_app'].' </div>';
+    echo '<div style="font-weight: 900;font-variant: all-petite-caps;font-size: 18px;">'.$language['all_my_apps'].': <b>'.$CountApps.'</b></div>';
     ?>
   </div>
   <div style="padding: 10px; border-bottom: 2px dashed #ccc;">
@@ -313,9 +316,9 @@ if(!empty($AppSearch)){
     }
 
     if($AppEditMode){
-      $AppTabCaption = 'Обновить \ Изменить приложение';
+      $AppTabCaption = $language['app_tab_mode_1'];
     }else{
-      $AppTabCaption = 'Публикация приложения';
+      $AppTabCaption = $language['app_tab_mode_2'];
     }
 
     if($AppEditMode){
@@ -386,34 +389,34 @@ if(!empty($AppSearch)){
       showError($GetStatusUpload);
     }
 
-    echo '<div>Имя приложения(латиница):</div>';
-    $gui->inputslabel('', 'text', 'name'.$AppID, $u_name,'70', 'Имя приложения');
+    echo '<div>'.$language['pub_name'].' ('.$language['pub_lat'].'):</div>';
+    $gui->inputslabel('', 'text', 'name'.$AppID, $u_name,'70', $language['pub_name']);
 
-    echo '<div>Имя приложения(кириллица):</div>';
-    $gui->inputslabel('', 'text', 'second_name'.$AppID, $u_sname,'70', 'Имя приложения');
+    echo '<div>'.$language['pub_name'].' ('.$language['pub_cyr'].'):</div>';
+    $gui->inputslabel('', 'text', 'second_name'.$AppID, $u_sname,'70', $language['pub_name']);
 
-    echo '<div>Версия:</div>';
-    $gui->inputslabel('', 'text', 'version'.$AppID, $u_version,'70', 'Версия');
+    echo '<div>'.$language['version_caption'].':</div>';
+    $gui->inputslabel('', 'text', 'version'.$AppID, $u_version,'70', $language['version_caption']);
 
-    echo '<div>Версия ОС:</div>';
-    $gui->inputslabel('', 'text', 'os_version'.$AppID, $u_osversion,'70', 'Версия ОС');
+    echo '<div>'.$language['pub_os_ver'].':</div>';
+    $gui->inputslabel('', 'text', 'os_version'.$AppID, $u_osversion,'70', $language['pub_os_ver']);
 
-    echo '<div>Описание:</div>';
+    echo '<div>'.$language['description_caption'].':</div>';
     ?>
-    <textarea rows="10" id="description<? echo $AppID ?>" placeholder="Введите описание приложения" style="width: 70%; padding: 10px; margin: 10px 0; border: 1px solid #ccc;" name="description"><? echo $u_description ?></textarea>
+    <textarea rows="10" id="description<? echo $AppID ?>" placeholder="<? echo $language['pub_desc_caption'] ?>" style="width: 70%; padding: 10px; margin: 10px 0; border: 1px solid #ccc;" name="description"><? echo $u_description ?></textarea>
     <?
 
-    echo '<div>Приложение (zip):</div>';
-    $AppGUI->SelectFile($AppID, "file_url", "70%", "zip" , 'Файл приложения (zip)', 'Выбрать файл');
+    echo '<div>'.$language['install_n_2'].' (zip):</div>';
+    $AppGUI->SelectFile($AppID, "file_url", "70%", "zip" , $language['pub_file_caption'].' (zip)', $language['pub_select_file']);
 
-    echo '<div>Иконка (png):</div>';
-    $AppGUI->SelectFile($AppID, "icon_url", "70%", "png" , 'Файл иконки (png)', 'Выбрать файл');
+    echo '<div>'.$language['pub_icon'].' (png):</div>';
+    $AppGUI->SelectFile($AppID, "icon_url", "70%", "png" , $language['pub_icon_caption'].' (png)', $language['pub_select_file']);
 
     if(!$AppEditMode){
-      echo '<div id="PublishApp'.$AppID.'" onClick="PublishNewApp'.$AppID.'();" class="ui-forest-button ui-forest-accept" style="margin:30 auto;"> Загрузить </div>';
+      echo '<div id="PublishApp'.$AppID.'" onClick="PublishNewApp'.$AppID.'();" class="ui-forest-button ui-forest-accept" style="margin:30 auto;"> '.$language['pub_load_app'].' </div>';
     }else{
-      echo '<div id="UpdateApp'.$AppID.'" onClick="UpdateApp'.$AppID.'();" class="ui-forest-button ui-forest-accept" style="margin:30 auto;"> Обновить </div>';
-      echo '<div id="DeleteApp'.$AppID.'" messageTitle="Удалить это приложение?" messageBody="Внимание! Это приложение будет удалено" okButton="Удалить" cancelButton="Отмена" onClick="ExecuteFunctionRequest'.$AppID.'(this, \'DeleteApp'.$AppID.'\')" class="ui-forest-button ui-forest-cancel" style="margin:30 auto;"> Удалить </div>';
+      echo '<div id="UpdateApp'.$AppID.'" onClick="UpdateApp'.$AppID.'();" class="ui-forest-button ui-forest-accept" style="margin:30 auto;"> '.$language['pub_update_app'].' </div>';
+      echo '<div id="DeleteApp'.$AppID.'" messageTitle="'.$language['delete_mesage_t'].'" messageBody="'.$language['delete_mesage_b'].'" okButton="'.$language['pub_delete_app'].'" cancelButton="'.$language['delete_mesage_cancel'].'" onClick="ExecuteFunctionRequest'.$AppID.'(this, \'DeleteApp'.$AppID.'\')" class="ui-forest-button ui-forest-cancel" style="margin:30 auto;"> '.$language['pub_delete_app'].' </div>';
     }
 
     ?>
@@ -421,7 +424,7 @@ if(!empty($AppSearch)){
 </div>
 
 <div id="Updates<? echo $AppID ?>" style="margin: 0 auto; overflow: auto;">
-  <div id="TabTitle">Обновления</div>
+  <div id="TabTitle"><? echo $language['update_tab'] ?></div>
   <div>
     <?
 
@@ -443,17 +446,17 @@ if(!empty($AppSearch)){
       $OSDescription = $OSUpdateURL['0']['description'];
 
       if(empty($OSDescription)){
-        $OSDescription = 'Описание отсутствует';
+        $OSDescription = $language['os_null_description'];
       }
 
       echo '<div style="color: #ebece6; border-radius: 5px; padding: 20px; border: 1px solid #ccc; background: #2c2d3c; margin: 10px;">';
-      echo '<div id="TabTitle" style="font-size: 23px; font-weight: 600; color: #53e579;"> Обновление системы </div>';
+      echo '<div id="TabTitle" style="font-size: 23px; font-weight: 600; color: #53e579;"> '.$language['os_update_caption'].' </div>';
       echo '<p style="text-align:left; background-image: url(http://forest.hobbytes.com/media/os/updates/uplogo.png); background-size:cover; height:80px; width:80px;"></p>';
       echo '<span style="font-size:17px;"><b>Forest OS</b> '.$OSUpdateURL['0']['codename'].'</span><br>';
-      echo '<span style="font-size:12px; font-weight:900; " >сборка: <span style="color:#ebece6; text-transform: uppercase;">'.$OSUpdateURL['0']['file'].'</span></span><br>';
-      echo '<span style="font-size:12px; ">версия: '.$OSUpdateURL['0']['version'].'<br>версия сборки: '.$OSUpdateURL['0']['subversion'].'<br>размер: '.$format.'</span></span>';
-      echo '<br><br><b>Описание:</b><br><div style="font-size:15px; color:#ebece6; white-space:pre-wrap; padding: 4px 0px;">'.$OSDescription.'</div>';
-      echo '<div id="'.$OSUpdateURL['0']['file'].'" class="ui-forest-blink" t="app_h" onClick="update'.$AppID.'()" style="background-color:#962439; color:#fff; width:30%; margin: 70px auto 10px auto; font-size:15px; padding:10px; border-radius:5px; text-align:center;">Обновить</div>';
+      echo '<span style="font-size:12px; font-weight:900; " >'.$language['os_revision'].': <span style="color:#ebece6; text-transform: uppercase;">'.$OSUpdateURL['0']['file'].'</span></span><br>';
+      echo '<span style="font-size:12px; ">'.mb_strtolower($language['version_caption']).': '.$OSUpdateURL['0']['version'].'<br>'.$language['os_sub_version'].': '.$OSUpdateURL['0']['subversion'].'<br>'.mb_strtolower($language['size_caption']).': '.$format.'</span></span>';
+      echo '<br><br><b>'.$language['description_caption'].':</b><br><div style="font-size:15px; color:#ebece6; white-space:pre-wrap; padding: 4px 0px;">'.$OSDescription.'</div>';
+      echo '<div id="'.$OSUpdateURL['0']['file'].'" class="ui-forest-blink" t="app_h" onClick="update'.$AppID.'()" style="background-color:#962439; color:#fff; width:30%; margin: 70px auto 10px auto; font-size:15px; padding:10px; border-radius:5px; text-align:center;">'.$language['pub_update_app'].'</div>';
       echo '</div>';
       echo '<div style="border-bottom: 1px solid #ccc; padding: 10px; margin-bottom: 36px;"></div>';
     }
@@ -503,18 +506,18 @@ if(!empty($AppSearch)){
           echo str_replace('_', ' ', $GetComApps[$key]['name']);
           echo '</div>';
           echo '<span>';
-          echo 'Автор: '.$GetComApps[$key]['author'].'<br>';
-          echo 'Версия: '.$GetComApps[$key]['version'].'<br>';
-          echo 'Размер: '.$size.'<br>';
+          echo $language['autor_caption'].': '.$GetComApps[$key]['author'].'<br>';
+          echo $language['version_caption'].': '.$GetComApps[$key]['version'].'<br>';
+          echo $language['size_caption'].': '.$size.'<br>';
           echo '</span>';
           echo '<div style="padding: 25px 60px;">';
 
           if($CurrentVersionOS < $GetComApps[$key]['os_version']){
             echo '<div class="AppTile-button A-button-open" style="width: max-content;">';
-            echo 'Обновите ОС';
+            echo $language['need_update_button'];
           }else{
             echo '<div update="true" app="'.$GetComApps[$key]['name'].'" app_second="'.$GetComApps[$key]['second_name'].'" hash="'.$GetComApps[$key]['hash'].'" class="AppTile-button A-button-install">';
-            echo 'Обновить';
+            echo $language['pub_update_app'];
           }
 
           echo '</div>';
@@ -531,7 +534,7 @@ if(!empty($AppSearch)){
     }
 
     if($showEmpty){
-      echo '<div style="padding: 10px; font-size: 17px; text-align: center; color: #3c3c3c;">Обновления отсутствуют</div>';
+      echo '<div style="padding: 10px; font-size: 17px; text-align: center; color: #3c3c3c;">'.$language['updates_null'].'</div>';
     }
     ?>
   </div>
@@ -703,4 +706,5 @@ CheckUpdateLabel<?echo $AppID?>();
 
 //ScrollWindow
 $('#<?echo $AppName.$AppID?>').scrollTop(<?echo $ScrollTo?>);
+
 </script>
