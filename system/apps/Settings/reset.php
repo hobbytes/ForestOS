b54d1c85f605b5d0e74d6876f85dbfbe19053a8f
VG <gvgsochi@gmail.com>
2019-07-26 17:01:25 +0300

    допилил функцию удаления ОС

--- system/apps/Settings/reset.php
+++ system/apps/Settings/reset.php
@@ -16,8 +16,11 @@ $AppContainer = new AppContainer;
 $AppContainer->appName = $AppName;
 $AppContainer->appID = $AppID;
 $AppContainer->userControl = true;
-$AppContainer->LibraryArray = Array('bd', 'http');
+$AppContainer->LibraryArray = Array('bd', 'filesystem');
 $AppContainer->StartContainer();
+
+/* Make new objects */
+$FileAction = new fileaction;
 
 /* Delete system */
 $DeleteStatus = $AppContainer->GetAnyRequest('reset');
@@ -25,26 +28,65 @@ $DeleteStatus = $AppContainer->GetAnyRequest('reset');
 if($DeleteStatus){
 
   $bd = new readbd;
-  $HttpRequest = new http;
   $UsersList = $bd->GetAllUsers();
+  $server_url = "http://forest.hobbytes.com/media/os/ubase/deleteuser.php";
 
   foreach ($UsersList as $key => $value) {
 
-    $server_url = "http://forest.hobbytes.com/media/os/ubase/deleteuser.php";
     $password = $UsersList[$key]['password'];
     $fuid = $UsersList[$key]['fuid'];
 
     $userhash = md5($fuid.$_SERVER['DOCUMENT_ROOT'].$password);
-    $HttpRequest->makeNewRequest($server_url, 'Forest OS', $data = array('fuid' => "$fuid", 'followlink' => $_SERVER['SERVER_NAME'], 'userhash' => "$userhash"), "GET");
+
+    $data = http_build_query(array('fuid' => "$fuid", 'followlink' => $_SERVER['SERVER_NAME'], 'userhash' => "$userhash"));
+    $check = file_get_contents($server_url.'?'.$data);
 
   }
+
   $conn = new PDO (DB_DSN, DB_USERNAME, DB_PASSWORD);
   $conn->query("TRUNCATE TABLE forestusers");
+
   unset($password, $fuid, $userhash);
+
+  $curlCh = curl_init();
+  curl_setopt($curlCh, CURLOPT_URL, "http://forest.hobbytes.com/media/os/installer.zip");
+  curl_setopt($curlCh, CURLOPT_RETURNTRANSFER, 1);
+  curl_setopt($curlCh, CURLOPT_SSLVERSION,3);
+  $curlData = curl_exec($curlCh);
+  curl_close($curlCh);
+
+  $TempFile = '../../../installer.zip';
+  $file = fopen($TempFile, "w+");
+  fputs($file, $curlData);
+  fclose($file);
+
+  $zip = new ZipArchive;
+  if($zip->open($TempFile) === TRUE){
+    $zip->extractTo('../../../');
+    $zip->close();
+    unlink($TempFile);
+
+    $FileAction->deleteDir('../../../system');
+
+    foreach (glob('../../../{,.}*', GLOB_BRACE) as $file){
+      if ($file != "../../../index.php"){
+        unlink($file);
+      }
+    }
+  }
+
+  ?>
+
+  <script language="JavaScript">
+  setTimeout(function () {
+    window.location.replace("http://<?php echo $_SERVER['SERVER_NAME'] ?>");
+  }, 5000);
+  </script>
+
+  <?php
 
 }
 
-/* Make new objects */
 ?>
 <div style="width:100%; text-align:left; padding-bottom:10px; font-size:30px; border-bottom:#d8d8d8 solid 2px; text-overflow:ellipsis; overflow:hidden;">
 <span onClick="back<? echo $AppID ?>();" class="ui-forest" style="background-color:#d8d8d8; color:#000; border-radius:30%; cursor:pointer; font-size:25px; margin-left:5px;"> &#9668; </span><?echo $language[$_SESSION['locale'].'_feedback']?></div>
@@ -53,19 +95,26 @@ if($DeleteStatus){
 echo '<div style="text-align:left; margin-top:10px; margin-left:10px;">';
 echo '<b style="font-size:20px;">Удаление системы</b>';
 
-echo '<p style=" border: 2px dashed #dc4444; padding: 10px; background: #ff8787; color: #3e0b0b; font-weight: 600; margin: 15px auto; width: 80%;
-">Внимание! Все данные, а также учетные записи всех пользователей будут безвозвратно удалены!</p>';
+if(!$DeleteStatus){
+  echo '<p style=" border: 2px dashed #dc4444; padding: 10px; background: #ff8787; color: #3e0b0b; font-weight: 600; margin: 15px auto; width: 80%;
+  ">Внимание! Все данные, а также учетные записи всех пользователей будут безвозвратно удалены!</p>';
 
-echo '<div
-      messageTitle="Полное удаление системы"
-      messageBody="Вы уверены, что хотите полностью удалить систему?"
-      okButton="Удалить" cancelButton="Отмена"
-      onClick="ExecuteFunctionRequest'.$AppID.'(this, \'reset'.$AppID.'\')"
-      class="ui-forest-button ui-forest-cancel ui-forest-center">
-      Полное удаление системы
-</div><hr>';
+  echo '<div
+        messageTitle="Полное удаление системы"
+        messageBody="Вы уверены, что хотите полностью удалить систему?"
+        okButton="Удалить" cancelButton="Отмена"
+        onClick="ExecuteFunctionRequest'.$AppID.'(this, \'reset'.$AppID.'\')"
+        class="ui-forest-button ui-forest-cancel ui-forest-center">
+        Полное удаление системы
+  </div>';
+}else{
+  echo '<p style=" border: 2px dashed #dc4444; padding: 10px; background: #ff8787; color: #3e0b0b; font-weight: 600; margin: 15px auto; width: 80%;
+  ">ВНИМАНИЕ! ЗАПУЩЕН ПРОЦЕСС УДАЛЕНИЯ <i>FOREST OS</i>. Страница автоматически обновится через 5 секунд.</p>';
+}
 
-echo '</div>';
+
+
+echo '<hr></div>';
 
 $AppContainer->EndContainer();
 ?>
--- system/core/library/etc/http.php
+++ system/core/library/etc/http.php
@@ -2,14 +2,14 @@
 
 class http{
 
-  function makeNewRequest($url, $agent, $data, $requestType = "POST"){
+  function makeNewRequest($url, $agent, $data){
     $curl = curl_init();
     curl_setopt_array($curl, array(
         CURLOPT_RETURNTRANSFER => 1,
         CURLOPT_URL => $url,
         CURLOPT_USERAGENT => $agent,
         CURLOPT_FOLLOWLOCATION => true,
-        CURLOPT_CUSTOMREQUEST => $requestType,
+        CURLOPT_CUSTOMREQUEST => "POST",
         CURLOPT_POSTREDIR => 3,
         CURLOPT_POSTFIELDS => $data,
         CURLOPT_REFERER => $_SERVER ['HTTP_HOST'],
