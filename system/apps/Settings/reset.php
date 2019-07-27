7f0816b932de5cd537791a4cc915350d3e8f1ee3
VG <gvgsochi@gmail.com>
2019-07-27 10:10:52 +0300

    Теперь можно сносить систему к херам

--- system/apps/Settings/reset.php
+++ system/apps/Settings/reset.php
@@ -6,9 +6,6 @@ $AppID  = $_GET['appid'];
 $AppName  = $_GET['appname'];
 $Folder = $_GET['destination'];
 
-/* get localization file */
-$language  = parse_ini_file('lang/feedback.lang');
-
 require $_SERVER['DOCUMENT_ROOT'].'/system/core/library/Mercury/AppContainer.php';
 
 /* Make new container */
@@ -21,6 +18,9 @@ $AppContainer->StartContainer();
 
 /* Make new objects */
 $FileAction = new fileaction;
+
+/* Get localization file */
+$language = parse_ini_file('lang/reset.lang');
 
 /* Delete system */
 $DeleteStatus = $AppContainer->GetAnyRequest('reset');
@@ -39,7 +39,7 @@ if($DeleteStatus){
     $userhash = md5($fuid.$_SERVER['DOCUMENT_ROOT'].$password);
 
     $data = http_build_query(array('fuid' => "$fuid", 'followlink' => $_SERVER['SERVER_NAME'], 'userhash' => "$userhash"));
-    $check = file_get_contents($server_url.'?'.$data);
+    $request = file_get_contents($server_url.'?'.$data);
 
   }
 
@@ -89,27 +89,27 @@ if($DeleteStatus){
 
 ?>
 <div style="width:100%; text-align:left; padding-bottom:10px; font-size:30px; border-bottom:#d8d8d8 solid 2px; text-overflow:ellipsis; overflow:hidden;">
-<span onClick="back<? echo $AppID ?>();" class="ui-forest" style="background-color:#d8d8d8; color:#000; border-radius:30%; cursor:pointer; font-size:25px; margin-left:5px;"> &#9668; </span><?echo $language[$_SESSION['locale'].'_feedback']?></div>
+<span onClick="back<? echo $AppID ?>();" class="ui-forest" style="background-color:#d8d8d8; color:#000; border-radius:30%; cursor:pointer; font-size:25px; margin-left:5px;"> &#9668; </span><?echo $language[$_SESSION['locale'].'_reset']?></div>
 <?php
 
 echo '<div style="text-align:left; margin-top:10px; margin-left:10px;">';
-echo '<b style="font-size:20px;">Удаление системы</b>';
+echo '<b style="font-size:20px;">'.$language[$_SESSION['locale'].'_reset_label'].'</b>';
 
 if(!$DeleteStatus){
   echo '<p style=" border: 2px dashed #dc4444; padding: 10px; background: #ff8787; color: #3e0b0b; font-weight: 600; margin: 15px auto; width: 80%;
-  ">Внимание! Все данные, а также учетные записи всех пользователей будут безвозвратно удалены!</p>';
+  ">'.$language[$_SESSION['locale'].'_reset_warning_1'].'</p>';
 
   echo '<div
-        messageTitle="Полное удаление системы"
-        messageBody="Вы уверены, что хотите полностью удалить систему?"
-        okButton="Удалить" cancelButton="Отмена"
+        messageTitle="'.$language[$_SESSION['locale'].'_messageTitle'].'"
+        messageBody="'.$language[$_SESSION['locale'].'_messageBody'].'"
+        okButton="'.$language[$_SESSION['locale'].'_okButton'].'" cancelButton="'.$language[$_SESSION['locale'].'_cancelButton'].'"
         onClick="ExecuteFunctionRequest'.$AppID.'(this, \'reset'.$AppID.'\')"
         class="ui-forest-button ui-forest-cancel ui-forest-center">
-        Полное удаление системы
+        '.$language[$_SESSION['locale'].'_messageTitle'].'
   </div>';
 }else{
   echo '<p style=" border: 2px dashed #dc4444; padding: 10px; background: #ff8787; color: #3e0b0b; font-weight: 600; margin: 15px auto; width: 80%;
-  ">ВНИМАНИЕ! ЗАПУЩЕН ПРОЦЕСС УДАЛЕНИЯ <i>FOREST OS</i>. Страница автоматически обновится через 5 секунд.</p>';
+  ">'.$language[$_SESSION['locale'].'_reset_warning_2'].'</p>';
 }
 
 
