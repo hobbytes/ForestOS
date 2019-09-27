dc2fe6a28286a769fb06430191db4b116dc31790
VG <gvgsochi@gmail.com>
2019-09-27 15:35:56 +0300

    Добавил разделители для дока, осталось реализовать редактирование дока через настройки. На данный момент добавляется все вручную

--- system/core/library/dock.php
+++ system/core/library/dock.php
@@ -9,19 +9,40 @@ class Dock {
     }
 
     if(isset($_SESSION['loginuser'])){
+
       $dir = $_SERVER['DOCUMENT_ROOT'].'/system/users/'.$_SESSION["loginuser"].'/settings/Dock/';
       if(!is_dir($dir))
       {
         mkdir($dir);
+        if(!is_dir($dir.'A/')){
+          mkdir($dir.'A/');
+          file_put_contents($dir.'A/Explorer', "");
+          file_put_contents($dir.'A/Settings', "");
+          file_put_contents($dir.'A/Apps_House', "");
+        }
       }
+
+      $CountFolders = count( glob("$dir/*", GLOB_ONLYDIR) ); // > 1
+      $Temp = "";
+      $SeparatorCount = 0;
+
       echo '<div class="dock-container topbartheme">';
-      foreach (glob($dir.'*') as $object)
+      foreach (glob($dir.'*/*') as $object)
       {
         $object = pathinfo($object);
         $ObjectName = $object['filename'];
         $ObjectExt = $object['extension'];
         $ObjectApp = "/home/u588238148/public_html/forestos/system/apps/$ObjectName/main.php";
         if(is_file($ObjectApp)){
+
+          if($CountFolders >= 2 && $Temp != $object['dirname']){
+            $SeparatorCount++;
+            if($SeparatorCount > 1){
+              echo '<span class="dock-separator"></span>';
+            }
+            $Temp = $object['dirname'];
+          }
+
           $Action = "makeprocess('/home/u588238148/public_html/forestos/system/apps/$ObjectName/main.php', '$ObjectName', '', '$ObjectName');";
           echo '<div class="dock-icon" onClick="'.$Action.'" style="background-image: url(./system/apps/'.$ObjectName.'/app.png?h=1ca7108a11d44b16d38fce45ef19670b);">';
           echo '</div>';
